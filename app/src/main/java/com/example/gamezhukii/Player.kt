3aa911e27537dd3de44af6d7b7c22169f56d767c
Шатохин Роман Алексеевич <ip311s28@csc.sibsutis.ru>
2026-09-15 16:00:00 +0300

    Add player data model

---
+++ app/src/main/java/com/example/gamezhukii/Player.kt
@@ -0,0 +1,10 @@
+package com.example.gamezhukii
+
+data class Player(
+    val fullName: String,
+    val gender: String,
+    val course: Int,
+    val difficulty: Int,
+    val birthDate: String,
+    val zodiacSign: String
+)
